fe5cd0c138726481c7a471ac0b00a3f7fc7a1be2
Tisha Holder <50963507+TishaHolder@users.noreply.github.com>
2019-10-01 16:18:50 -0400

    completed post requests

--- postRoutes.js
+++ postRoutes.js
@@ -57,6 +57,41 @@ postRouter.post ('/', (req, res) => {
 //same as /api/posts/:id/comments
 postRouter.post('/:id/comments', (req, res) => {
 
+    const commentId = req.params.id;
+    const commentInformation = req.body;
+
+    if(commentInformation.text === ""){
+        res.status(400).json( {errorMessage: 'Please provide text for the comment.'} );
+
+    }
+    else {
+        DB.findById(commentId)
+        .then(post => {
+            if(!post){
+                res.status(404).json( {message: 'The post with the specified ID does not exist.'} );
+            }
+            else {
+                DB.insertComment(commentInformation)
+                .then(commentIdObject => {
+
+                    console.log("comment id object", commentIdObject);
+
+                    DB.findCommentById(commentIdObject.id)
+                    .then(comment => {
+                    
+                        res.status(201).json(comment);
+                    })  
+                    .catch(error => {
+                        res.status(500).json( {message: 'There was an error while saving the comment to the database.'})
+                    })                 
+
+                })
+                
+            }
+        })
+
+    }
+
 });
 
 //same as /api/posts/:id
